92953b411eb9589c9b8589ebefcf8aaeb32046d9
TheMrguiller <guvillat@opendeusto.es>
2022-11-11 22:25:02 +0100

    Ordenar los ficheros

--- proyect_haarcascades.py
+++ proyect_haarcascades.py
@@ -12,27 +12,29 @@ def shape_to_np(shape, dtype="int"):
     
 
 vid = cv2.VideoCapture(0) 
-
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+predictor = dlib.shape_predictor('facial-landmarks-recognition-master/shape_predictor_68_face_landmarks.dat')
 while(True):
 
     ret, frame = vid.read()
-    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.02,10)
     rects=[]
     for (x, y, w, h) in faces:
-        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        predictor = dlib.shape_predictor('facial-landmarks-recognition-master/shape_predictor_68_face_landmarks.dat')
-        rects.append((x, y, w, h))
-    for (i, rect) in enumerate(rects):
+        frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        rect= dlib.rectangle(x, y, w, h) 
+        #rects.append((x, y, w, h))
+    #for (i, rect) in enumerate(rects):
         shape = predictor(gray, rect)
         shape = shape_to_np(shape)
         for (xs, ys) in shape:
-            cv2.circle(img, (xs, ys), 2, (0, 0, 255), -1)
+            cv2.circle(frame, (xs, ys), 2, (0, 0, 255), -1)
 
     cv2.namedWindow('eye')
-    cv2.imshow('eye', img)
+    cv2.imshow('eye', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'): # 16 bits mayuscula 8 bits minuscula
         break;
+
 vid.release()# Soltamos el objeto captura
 cv2.destroyAllWindows() # Destruimos todas las ventanas generadas
