33fde2183e23a1c593486c779f98e59b8f481cab
AsierGonz <113672888+AsierGonz@users.noreply.github.com>
2022-11-22 17:43:45 +0100

    Ventana Funcional
    ventana funcionando

--- applicacionUi.py
+++ applicacionUi.py
@@ -13,14 +13,6 @@ from PyQt5.uic import loadUi
 from mainUi import Ui_MainWindow
 
 
-class FindReplaceDialog(QDialog):
-
-    def __init__(self, parent=None):
-
-        super().__init__(parent)
-
-        loadUi("ui/find_replace.ui", self)
-
 
 class Window(QMainWindow, Ui_MainWindow):
 
@@ -29,32 +21,6 @@ class Window(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
 
         self.setupUi(self)
-
-
-    def findAndReplace(self):
-
-        dialog = FindReplaceDialog(self)
-
-        dialog.exec()
-
-
-    def about(self):
-
-        QMessageBox.about(
-
-            self,
-
-            "About Sample Editor",
-
-            "<p>A sample text editor app built with:</p>"
-
-            "<p>- PyQt</p>"
-
-            "<p>- Qt Designer</p>"
-
-            "<p>- Python</p>",
-
-        )
 
 
 
--- mainUi.py
+++ mainUi.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+import os
 
 class Ui_MainWindow(object):
 
@@ -10,12 +11,24 @@ class Ui_MainWindow(object):
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(50, 90, 171, 91))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.funcionBoton1)
+
+
+
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(300, 90, 171, 91))
         self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.funcionBoton2)
+
+
+
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setGeometry(QtCore.QRect(540, 90, 171, 91))
         self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.clicked.connect(self.funcionBoton3)
+
+
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -30,4 +43,18 @@ class Ui_MainWindow(object):
         self.pushButton.setText(_translate("MainWindow", "Tracker"))
         self.pushButton_2.setText(_translate("MainWindow", "Calibrar"))
         self.pushButton_3.setText(_translate("MainWindow", "Salir"))
+
+
+    def funcionBoton1(self):
+        os.system("python pintarSobreImagen_v2.py")
+        print("Has clicado al boton1")
+
+    def funcionBoton2(self):
+        os.system("python calibrar.py")
+        print("Has clicado al boton2")
+
+    def funcionBoton3(self):
+        print("Has clicado al boton3")
+        self.close() 
+
 
