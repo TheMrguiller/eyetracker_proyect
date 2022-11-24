fed3a5b05fe398cd8bc386cd42c4d16d687b9a40
AsierGonz <113672888+AsierGonz@users.noreply.github.com>
2022-11-24 11:46:51 +0100

    Se escribem leen y miran la resolu de puntos
    Eso qe digo en el titulo

--- calibrar.py
+++ calibrar.py
@@ -1,4 +1,4 @@
-
+import subprocess
 from imutils import face_utils
 import dlib
 import cv2
@@ -6,11 +6,19 @@ import numpy as np
 from helpers import *
 import json
 import os
+from screeninfo import get_monitors
 
+
+
+def get_screen_resolution():
+    output = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
+    resolution = output.split()[0].split(b'x')
+    return {'width': resolution[0], 'height': resolution[1]}
 
 def onMouse(event, x, y, flags, param):
     global indice
     global copia
+    
     if event == cv2.EVENT_LBUTTONDOWN:
         if indice >= 9:
             indice=0
@@ -19,7 +27,9 @@ def onMouse(event, x, y, flags, param):
             if indice >= 9:
                 indice=0
             copia=cv2.rectangle(blank_image.copy(), lista[indice][0], lista[indice][1], (255,255,255), -1)
-            get_positions(image=image,indice=indice)
+            file = open('config.txt', 'a')
+            file.write(get_positions(image=image,indice=indice) + '\n')
+            file.close()
             
 def create_points_list(height,width):
     seccion0= [(0,0),(50,50)]
@@ -35,8 +45,7 @@ def create_points_list(height,width):
 
 def get_positions(image,indice):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        
-    # detect faces in the grayscale image
+    print(indice)
     rects = detector(gray, 0)
     rect=get_frontal_face(rects)
     if rect ==0:
@@ -46,46 +55,71 @@ def get_positions(image,indice):
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
         cX_r,cY_r,crop_img_r,cX_l,cY_l,crop_img_l=eye_detector.get_eyes_position(image=image,shape=shape)
-        check_json(indice,cX_r,cY_r,crop_img_r,cX_l,cY_l,crop_img_l)
+        return write_txt(cX_r,cY_r,crop_img_r,cX_l,cY_l,crop_img_l)
 
-def check_json(indice,cX_r,cY_r,crop_img_r,cX_l,cY_l,crop_img_l):
-    isFile = os.path.isfile("config.txt")
-    print(isFile)
-    if isFile == True:
-        write_txt(indice,cX_r,cY_r,crop_img_r,cX_l,cY_l,crop_img_l,write_type="r+")
-    else:
-        write_txt(indice,cX_r,cY_r,crop_img_r,cX_l,cY_l,crop_img_l,write_type="w")
+def createTXT():
+    f = open('config.txt', 'w')
+    f.close()
 
-def write_txt(cX_r,cY_r,crop_img_r,cX_l,cY_l,crop_img_l,write_type): 
+def write_txt(cX_r,cY_r,crop_img_r,cX_l,cY_l,crop_img_l): 
     cadenaCompleta = "cX_r:" + str(cX_r) + ',' + "cY_r:" + str(cY_r) + ',' + "w_r:" + str(crop_img_r.shape[1]) + ',' + "h_r:" + str(crop_img_r.shape[0]) + ',' + "cX_l:" + str(cX_l) + ',' + "cY_l:" + str(cY_l) + ','"w_l:" + str(crop_img_l.shape[1]) + ',' +  "h_l:" + str(crop_img_l.shape[0])
+    return cadenaCompleta
 
-    print(cadenaCompleta)    
-    with open("config.json",write_type) as file:
-        file.write(cadenaCompleta)
-    file.close()
+def readTxt():
 
-indice=0
-blank_image = np.zeros((500,1000,3), np.uint8)
-copia = blank_image.copy()
-height = blank_image.shape[0]
-width = blank_image.shape[1]
-lista=create_points_list(height=height,width=width)
-cv2.rectangle(copia, lista[0][0], lista[0][1], (255,255,255), -1)
-cv2.namedWindow('MyWindow')
-cv2.setMouseCallback('MyWindow', onMouse)
-p = "facial-landmarks-recognition-master/shape_predictor_68_face_landmarks.dat"
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(p)
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS, 60)
-eye_detector=eyeDetector()
-while True:
-    # load the input image and convert it to grayscale
-    _, image = cap.read()
-    cv2.imshow("MyWindow",copia)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'): # escape when q is pressed
-        break
+    lineasPunto =  []
+    with open('config.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            lineaUnica = []
+            line = line.strip()
+            splited = line.split(",")
+            #print(splited)
+            for tupla in splited:
+                #print(tupla)
+                tupla = tupla.split(":")
+                #print(tupla[1])  ##todos los valores de una linea
+                lineaUnica.append(int(tupla[1]))
+            lineasPunto.append(lineaUnica)
+        print(lineasPunto)
+        f.close()
+
+
+if __name__ == "__main__":
+
+    indice=0
+
+    width=1920 
+    height=1080
+    for m in get_monitors():
+        width = m.width
+        height = m.height
+
+    width = int(width * 0.91)
+    height = int(height * 0.91)
+    size = get_screen_resolution()
+    blank_image = np.zeros((height,width,3), np.uint8)
+    copia = blank_image.copy()
+    height = blank_image.shape[0]
+    width = blank_image.shape[1]
+    file = createTXT()
+    lista=create_points_list(height=height,width=width)
+    cv2.rectangle(copia, lista[0][0], lista[0][1], (255,255,255), -1)
+    cv2.namedWindow('MyWindow')
+    cv2.setMouseCallback('MyWindow', onMouse)
+    p = "facial-landmarks-recognition-master/shape_predictor_68_face_landmarks.dat"
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(p)
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, 60)
+    eye_detector=eyeDetector()
+    while True:
+        _, image = cap.read()
+        cv2.imshow("MyWindow",copia)
         
-cv2.destroyAllWindows()
-cap.release()
+        if cv2.waitKey(1) & 0xFF == ord('q'): # escape when q is pressed
+            readTxt()
+            break
+            
+    cv2.destroyAllWindows()
+    cap.release()
